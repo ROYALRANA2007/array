2081a6ff5e014ae0e9f06edf2243eeb389cd8b13
Royal rana <royalrana2007@gmail.com>
2026-01-26 14:09:56 +0530

    Add files via upload

---
+++ all_repeating_elements.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void all_non_repeating_elements(int arr[], int n)
+{
+    int i, j, visited[100]={0};
+    for (i = 0; i < n; i++)
+    {
+        int count = 0, cur = arr[i];
+        
+        if (visited[arr[i]] != 1)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (arr[j] == cur)
+                {
+                    count++;
+                }
+            }
+        }
+            if(count>1)
+        printf(" %d",arr[i]);
+        visited[arr[i]]=1;
+        
+        
+        
+    }
+}
+    int main()
+    {
+        int arr[]={8,85,4,2,4,59,7,2,54,7,6,9,6,3,3,2};
+        int n=sizeof arr/sizeof arr[0];
+        all_non_repeating_elements(arr,n);
+        return 0;
+    }
